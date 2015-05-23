498b64526afcfd2915b362c52a05a8eced649295
rictyran <rictyran@gmail.com>
2015-05-22 23:06:10 -0400

    Kids menu planning app built in Swift
    work in progress

---
+++ Kids Menu Planner/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  Kids Menu Planner
+//
+//  Created by Richard Tyran on 5/22/15.
+//  Copyright (c) 2015 Richard Tyran. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Kids Menu Planner/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Kids Menu Planner
+//
+//  Created by Richard Tyran on 5/22/15.
+//  Copyright (c) 2015 Richard Tyran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBAction func addProfileButtonPressed(sender: AnyObject) {
+    
+    
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
